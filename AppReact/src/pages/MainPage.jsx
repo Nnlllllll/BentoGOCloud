2387c47455f9daad4cba1b9459449906552069f8
pepebragante <pepe.bragante@gmail.com>
2026-05-20 12:54:43 -0300

    Add files via upload

---
+++ AppReact/src/pages/MainPage.jsx
@@ -0,0 +1,39 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import styles from "../styles/Main.module.css";
+
+import modelPath from "../assets/Bentao.glb";
+
+function Modelo() {
+  const { scene } = useGLTF(modelPath);
+  return <primitive object={scene} scale={[8, 8, 8]} position={[0, -2, 0]} />;
+}
+
+export default function MainPage() {
+  return (
+<div style={{ position: 'relative', width: '100vw', height: '100vh' }}>
+  <div className={styles.container} style={{ width: '100%', height: '100%' }}>
+    <Canvas camera={{ position: [0, 0, 15] }}>
+      <ambientLight intensity={1.5} />
+      <pointLight position={[10, 10, 10]} />
+      <Suspense fallback={<div style={{color: 'white'}}>Carregando Mapa...</div>}>
+        <Modelo />
+      </Suspense>
+      <OrbitControls enableZoom={true} enablePan={true} />
+    </Canvas>
+  </div>
+
+  <div className={styles.buttonContainer}>
+    <button 
+      className={styles.buttonSecondary}
+      onClick={() => navigate("/settings")}
+      style={{ backgroundColor: '#c4c360', color: 'white' }}
+    >
+      Ir para configs
+    </button>
+  </div>
+</div>
+  );
+
+}
